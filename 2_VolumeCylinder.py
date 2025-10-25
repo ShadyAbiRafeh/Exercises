9aae6150019277193d2b58f773ae325ad958b1e4
Shady Abi Rafeh <abirafeh.s.06@gmail.com>
2025-10-25 12:50:01 +0200

    volume calculation

---
+++ 2_VolumeCylinder.py
@@ -0,0 +1,6 @@
+#volume of a cylinder
+height = float(input("Enter the height of the cylinder: "))
+radius = float(input("Enter the radius of the cylinder: "))
+pi = 3.14159
+volume = pi * (radius ** 2) * height
+print("The volume of the cylinder is: " + str(volume))
