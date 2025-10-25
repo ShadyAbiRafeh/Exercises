a5acf2d5ade959364ea4e96195c84a8e6751aa71
Shady Abi Rafeh <abirafeh.s.06@gmail.com>
2025-10-25 12:37:35 +0200

    compare with 0.3

---
+++ 2_DataType_FloatingPoint.py
@@ -0,0 +1,9 @@
+value1 = 0.1
+valu2 = 0.2
+result = value1 + valu2
+print("The result of adding", value1, "and", valu2, "is:", result)
+if result == 0.3:
+    print("The result is exactly 0.3")
+else:
+    print("The result is not exactly 0.3 due to floating-point precision issues.")
+    
