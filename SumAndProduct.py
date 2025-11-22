ed15cb1354e974969f4ab60d618beb754c44156c
Shady Abi Rafeh <abirafeh.s.06@gmail.com>
2025-11-22 10:03:53 +0100

    no message

---
+++ SumAndProduct.py
@@ -0,0 +1,10 @@
+print("even numbers")
+for i in range(2, 51, 2):
+    print(i, end=' ')
+print("\nusing while loop: ")
+num = 2
+while num <= 50:
+    print(num, end=' ')
+    num += 2
+print(
+    #continue idk how
