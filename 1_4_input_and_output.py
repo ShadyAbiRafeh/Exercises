fe6bdcc63ef586e78ba54c57e71a9261725a7b7b
Shady Abi Rafeh <abirafeh.s.06@gmail.com>
2025-11-22 10:11:22 +0100

    Reapply "Merge branch 'exercise/1_4_input_and_output' into FlowControl/SumAndProduct"
    This reverts commit 33f9405c0ff99a9e89a1f9257039b116f0c78328.

---
+++ 1_4_input_and_output.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")   
+greeting = input("Enter your greeting: ")
+print(greeting + ", " + name + "!")
+print(f"{greeting}, {name}!")
--- 1_introduction_Exercise2.py
+++ 1_introduction_Exercise2.py
@@ -1,1 +1,10 @@
 #Write a program that takes your name as input and prints a personalized greeting along with a calculation of your birth year.
+#input
+name = input ("Enter your name: ")
+print ("Hello, " + name + "!")
+age = int(input ("Enter your age: "))
+current_year = 2025
+birth_year = current_year - age
+print ("You where born in the year " + str(birth_year) + ".")
+
+
